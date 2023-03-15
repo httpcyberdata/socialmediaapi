68097e809be4275031d1885cbfef9582a42c4a0f
httpcyberdata <shieldedsafety@gmail.com>
2023-03-15 03:20:10 -0400

    chore: created signup async function for creating User w/ try/catch

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -8,11 +8,44 @@ export const getAllUser = async (req, res, next) => {
 		console.log(err);
 	}
 	if(!users) {
-		return res.status(404).json({ message: 'No Users Found' });
+		return res.status(404).json({ 
+			message: 'No Users Found' 
+		});
 	}
-	return res.status(200).json({ users })
+	return res.status(200).json({ 
+		users 
+	})
 };
 
-export const signup = async (req, rs, next) => {
-	// 26:24
+export const signup = async (req, res, next) => {
+	const { name, email, password } = req.body;
+
+	let existingUser;
+
+	try {
+		exisitingUser = await User.findOne({
+			email
+		})
+	} catch(err) {
+		console.log(err);
+	}
+	if(existingUser) {
+		return res.status(400).json({ 
+			message: "User already exists! Login instead."
+		})
+	}
+	const user = new User({
+		name,
+		email,
+		password
+	});
+
+	try {
+		await user.save();
+	} catch(err) {
+		console.log(err);
+	}
+	return res.status(201).json({
+		user
+	})
 }
